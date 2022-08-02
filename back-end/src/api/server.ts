2367bda6f1e6eb13250f3ff7b27b78cb92ce6937
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-08-02 04:36:37 -0300

    feat: add ranking router to server

--- back-end/src/api/server.ts
+++ back-end/src/api/server.ts
@@ -1,8 +1,9 @@
+/* eslint-disable max-len */
 import App from './app';
 
 import CompetitionController from './controllers/Competition.controller';
-// eslint-disable-next-line max-len
 import CompetitionResultsController from './controllers/CompetitionResult.controller';
+import RankingController from './controllers/Ranking.controller';
 
 import { Competition } from './interfaces/competition.interface';
 import { CompetitionResult } from './interfaces/competitionResult.interface';
@@ -13,14 +14,18 @@ const server = new App();
 
 const competitionController = new CompetitionController();
 const competitionResultsController = new CompetitionResultsController();
+const rankingController = new RankingController();
 
 const competitionRouter = new CustomRouter<Competition>();
 const competitionResultsRouter = new CustomRouter<CompetitionResult>();
+const rankingRouter = new CustomRouter();
 
 competitionRouter.addRoute(competitionController);
 competitionResultsRouter.addRoute(competitionResultsController);
+rankingRouter.addRoute(rankingController);
 
 server.addRouter(competitionRouter.router);
 server.addRouter(competitionResultsRouter.router);
+server.addRouter(rankingRouter.router);
 
 export default server;
