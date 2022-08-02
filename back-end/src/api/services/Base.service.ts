af902d9cf7ef5f13959d199040e0b016b36ef9d4
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-08-02 01:53:20 -0300

    feat: creates abstract classes for use in CRUD in MSC model

---
+++ back-end/src/api/services/Base.service.ts
@@ -0,0 +1,33 @@
+import { ZodError } from 'zod';
+// import { IModel } from '../interfaces/model.interface';
+
+export interface ServiceError {
+  error: ZodError;
+}
+
+abstract class Service<T> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(protected model: any) {}
+
+  public async create(obj: T): Promise<T | null | ServiceError> {
+    return this.model.create(obj);
+  }
+
+  public async findAll(): Promise<T[]> {
+    return this.model.findAll();
+  }
+
+  public async findByPk(id: string): Promise<T | null | ServiceError> {
+    return this.model.findByPk(id);
+  }
+
+  public async update(id: string, obj: T): Promise<T | null | ServiceError> {
+    return this.model.update(id, obj);
+  }
+
+  public async destroy(id: string): Promise<T | null | ServiceError> {
+    return this.model.destroy(id);
+  }
+}
+
+export default Service;
